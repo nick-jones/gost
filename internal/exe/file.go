bd299a64b0066928ba6e8a125a794834708b5c17
Nicholas Jones <me@nicholasjon.es>
2020-06-05 17:19:57 +0100

    Check for Mach-O magic

--- internal/exe/file.go
+++ internal/exe/file.go
@@ -1,6 +1,7 @@
 package exe
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 	"io"
@@ -20,12 +21,26 @@ type File interface {
 	io.Closer
 }
 
+var (
+	machoMagicLE = []byte{0xcf, 0xfa, 0xed, 0xfe}
+	machoMagicBE = []byte{0xfe, 0xed, 0xfa, 0xcf}
+)
+
 // Open opens the named file
 func Open(filePath string) (File, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
 		return nil, fmt.Errorf("unable to open file: %w", err)
 	}
-	// TODO: check for Mach-O / ELF / etc
-	return newMacho(f)
+
+	ident := make([]byte, 4)
+	if _, err := f.ReadAt(ident, 0); err != nil {
+		return nil, err
+	}
+
+	if bytes.Equal(ident, machoMagicLE) || bytes.Equal(ident, machoMagicBE) {
+		return newMacho(f)
+	}
+
+	return nil, fmt.Errorf("could not determine exe type for %s", filePath)
 }
